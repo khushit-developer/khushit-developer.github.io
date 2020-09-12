9ece7ee9cb522e4c1fbb95daf60b7ac20c156c12
khushit-developer <65764461+khushit-developer@users.noreply.github.com>
2020-09-12 20:37:56 +0530

    added new files

---
+++ homepage.js
@@ -0,0 +1,7 @@
+
+(function($){
+    "use strict";
+    $('.next').click(function(){ $('.carousel').carousel('next');return false; });
+    $('.prev').click(function(){ $('.carousel').carousel('prev');return false; });
+})	
+(jQuery);
